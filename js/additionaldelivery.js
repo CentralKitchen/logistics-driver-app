d023561677a63f4cdd5209206907f7c1e863c2e2
CentralKitchen <mohamad.jufri@middleton.edu.sg>
2026-08-04 15:57:06 +0800

    Add files via upload

---
+++ js/additionaldelivery.js
@@ -0,0 +1,15 @@
+document
+.getElementById("adhocBtn")
+.addEventListener("click",()=>{
+
+alert("Ad-hoc Delivery page coming next.");
+
+});
+
+document
+.getElementById("revisitBtn")
+.addEventListener("click",()=>{
+
+alert("Revisit Delivery page coming next.");
+
+});
